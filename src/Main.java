e06adb934332dffde9904f1102bf8c0abf4b8abc
DivyaTeja97 <31980488+DivyaTeja97@users.noreply.github.com>
2017-09-16 13:29:09 +0530

    1st

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args){
+		int id=1;
+		String name="divya";
+	    Date d=new Date();
+	    double marks=56;
+		Student s=new Student(id,name,d,marks);
+	
+	//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
